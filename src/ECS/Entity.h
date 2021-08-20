4cd572fabd3a366777b1b32ac352e019b5c05206
Absurdponcho <60100776+Absurdponcho@users.noreply.github.com>
2021-08-21 02:27:01 +1000

    Entity

--- src/ECS/Entity.h
+++ src/ECS/Entity.h
@@ -1,6 +1,14 @@
 #pragma once
+#include <vector>
+#include "Component.h"
 
 class Entity
 {
+public:
+	template<class T>
+	void AddComponent() { Components.push_back(new T()); }
+	void RemoveComponent(Component* Comp);
 
+private:
+	std::vector<Component*> Components;
 };
