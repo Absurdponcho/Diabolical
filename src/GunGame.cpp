6c2cfb18e3c029d41d5a7af83a5c630a6f77fd13
Sam <itsthatguyagain3@gmail.com>
2021-08-21 02:23:13 +1000

    Window manager is now functioning

--- src/GunGame.cpp
+++ src/GunGame.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "Logging/Logging.h"
+#include "WindowManager.h"
+#undef main
 
-int main()
+int main(int argc, char** argv)
 {
     Logging::Log("main", "Main function lel");
     Logging::LogWarning("main", "Main warning lel");
     Logging::LogError("main", "Main error lel");
+    WindowManager WManager = WindowManager(
+        "GunGame",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600, 0);
+
+    while (1);
+    return 0;
 }
