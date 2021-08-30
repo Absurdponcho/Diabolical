1e6357887527642d43130b86b50631c27bb12573
Sam <itsthatguyagain3@gmail.com>
2021-08-30 18:49:43 +1000

    Random stuff actually works properly, uses unsigned int now, you can cast it back to int if you need | Added GlobalRandom based on stdmin_rand0 from C++11

--- src/Utility/GameMath.cpp
+++ src/Utility/GameMath.cpp
@@ -1,6 +1,7 @@
 #include "GameMath.h"
 #include <ctime>
 #include <Windows.h>
+#include "..\GunGame.h"
 
 FastRandom::FastRandom()
 {
@@ -28,4 +29,45 @@ inline int FastRandom::GetNextRandom(int Low, int High)
 {
 	Seed = (214013 * Seed + 2531011);
 	return ((Seed >> 16) & 0x7FFF % ((High - Low) + 1) ) + Low;
+}
+
+GlobalRandom* GlobalRandom::Singleton;
+unsigned int GlobalRandom::GlobalSeed = 0;
+
+GlobalRandom::GlobalRandom(int Seed)
+{
+	GlobalSeed = Seed;
+}
+
+void GlobalRandom::Initialize()
+{
+	GlobalSeed = _time32(nullptr);
+	GlobalSeed ^= GetCurrentProcessId();
+}
+
+void GlobalRandom::Initialize(int Seed)
+{
+	static bool bInitialized = false;
+	Check(!bInitialized);
+	bInitialized = true;
+	Singleton = new GlobalRandom(Seed);
+}
+
+unsigned int GlobalRandom::Next()
+{
+	GlobalSeed = (Multiplier * GlobalSeed + Increment) % Modulus;
+	return GlobalSeed;
+}
+
+void GlobalRandom::Discard(unsigned int z)
+{
+	for (z; z > 0; z--)
+	{
+		GlobalSeed = (Multiplier * GlobalSeed + Increment) % Modulus;
+	}
+}
+
+GlobalRandom* GlobalRandom::Get()
+{
+	return Singleton;
 }
--- src/Utility/GameMath.h
+++ src/Utility/GameMath.h
@@ -11,3 +11,23 @@ private:
 	int Seed;
 };
 
+class GlobalRandom {
+	GlobalRandom() {};
+	GlobalRandom(int Seed);
+	// Don't Change Unless you want different LCG engine settings
+	static const unsigned int Modulus = 0x7FFFFFFF;
+	static const unsigned int Multiplier = 48271;
+	static const unsigned int Increment = 0;
+public:
+	static void Initialize();
+	static void Initialize(int Seed);
+	static unsigned int Next();
+	void Discard(unsigned int z);
+	static GlobalRandom* Get();
+
+
+private:
+	static GlobalRandom* Singleton;
+	static unsigned int GlobalSeed;
+};
+
