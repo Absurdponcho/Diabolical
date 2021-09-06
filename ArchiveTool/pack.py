81c07de42c0abf0f5181433958d136c61664eb09
Sam <itsthatguyagain3@gmail.com>
2021-09-06 10:47:09 +1000

    Now only compresses files if its more space efficient

--- ArchiveTool/pack.py
+++ ArchiveTool/pack.py
@@ -24,39 +24,50 @@ current_size = 0
 
 
 class Descriptor():
-    def __init__(self, name, uncompressed_size, size, data):
+    def __init__(self, name, uncompressed_size, size, data, is_compressed):
         self.name = name
         self.offset = len(header) + ((path_limit + 36) * number_of_files)
         self.offset += current_size
         self.size = size
         self.uncompressed_size = uncompressed_size
-        self.is_compressed = 1
+        self.is_compressed = is_compressed
         self.data = data
 
-
+number_of_compressed_files = 0
 print(f"Packing {number_of_files} files into archive\n")
-compress_ratio = 0
+compress_ratio_total = 0
 for file in file_list:
     with open(file, "rb") as f:
-        print(f"Adding and Compressing file {file}...")
         data = f.read()
+        data_size = 0
         data_uncompressed_size = len(data)
-        data = zlib.compress(data, 9)
-        data_size = len(data)
+        data_compressed = zlib.compress(data, 9)
+        data_compressed_size = len(data_compressed)
+        compress_ratio = (data_compressed_size / data_uncompressed_size) * 100
+        is_compressed = True
+        if compress_ratio < 100.0:
+            compress_ratio_total += compress_ratio
+            print(f"Adding and Compressing {file}...")
+            data = data_compressed
+            data_size = data_compressed_size
+            number_of_compressed_files += 1
+        else:
+            print(f"Adding Uncompressed {file}...")
+            data_size = data_uncompressed_size
+            is_compressed = False
+
         fdesc = Descriptor(
-            bytearray(file, "ASCII"), data_uncompressed_size, data_size, data)
+            bytearray(file, "ASCII"), data_uncompressed_size, data_size, data, is_compressed)
         if data_size == 0 or data_uncompressed_size == 0:
             print(f"error {file} empty!")
             exit()
         file_descriptors.append(fdesc)
-
-        compress_ratio += (data_size / data_uncompressed_size) * 100
         f.close()
         current_size += data_size
 
 descriptor_data = bytearray()
 archive_data = bytearray()
-print(f"\nBuilding archive {argv[2]} ({number_of_files} files)")
+print(f"\nBuilding archive {argv[2]} ({number_of_files} files, {number_of_compressed_files} compressed)")
 for fd in file_descriptors:
     if len(fd.name) > path_limit:
         print(f"error file name too long! - {fd.name}")
@@ -73,4 +84,4 @@ with open(argv[2], "wb") as f:
     f.write(archive)
     f.close()
 print(f"Done! Archive {argv[2]} is written to file")
-print(f"Compression Ratio Average {compress_ratio / number_of_files}%\n")
+print(f"Compression Ratio Average {compress_ratio_total / number_of_compressed_files}%\n")
