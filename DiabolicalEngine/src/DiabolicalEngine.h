c1879dd63f7242e1fe28acd437463069d22bde10
Absurdponcho <60100776+Absurdponcho@users.noreply.github.com>
2022-01-18 20:03:17 +1100

    Simple logging

--- DiabolicalEngine/src/DiabolicalEngine.h
+++ DiabolicalEngine/src/DiabolicalEngine.h
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string>
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#define PLATFORM_WINDOWS
+#endif
+
 class DEngine
 {
 public:
