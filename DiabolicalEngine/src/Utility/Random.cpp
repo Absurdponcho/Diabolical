b7c622d2d769110821d39a00bb6727610c289219
Sam <itsthatguyagain3@gmail.com>
2022-01-24 23:20:38 +1100

    remove DRand namespace to fit code conventions

--- DiabolicalEngine/src/Utility/Random.cpp
+++ DiabolicalEngine/src/Utility/Random.cpp
@@ -1,10 +1,10 @@
 #include "Random.h"
 
-DRand::FastRandom32::FastRandom32()
+FastRandom32::FastRandom32()
 {
     UInt32 seed = 1;
 
-    if (DRand::bAlwaysRandomizeSeed) {
+    if (bAlwaysRandomizeSeed) {
         srand(_time32(nullptr));
         seed = rand32() ^ 0xC;
     }
@@ -12,53 +12,53 @@ DRand::FastRandom32::FastRandom32()
     rand32.seed(seed);
 }
 
-DRand::FastRandom32::FastRandom32(UInt32 seed)
+FastRandom32::FastRandom32(UInt32 seed)
 {
     rand32.seed(seed);
 }
 
-UInt32 DRand::FastRandom32::RandomUInt()
+UInt32 FastRandom32::RandomUInt()
 {
     return (UInt32) rand32();
 }
 
-UInt32 DRand::FastRandom32::RandomUInt(UInt32 min, UInt32 max)
+UInt32 FastRandom32::RandomUInt(UInt32 min, UInt32 max)
 {
     return (UInt32) rand32() % (max - min) + min;
 }
 
-Int32 DRand::FastRandom32::RandomInt()
+Int32 FastRandom32::RandomInt()
 {
     return (Int32) rand32();
 }
 
-Int32 DRand::FastRandom32::RandomInt(Int32 min, Int32 max)
+Int32 FastRandom32::RandomInt(Int32 min, Int32 max)
 {
     return (Int32) rand32() % (max - min) + min;
 }
 
-Float32 DRand::FastRandom32::RandomFloat()
+Float32 FastRandom32::RandomFloat()
 {
     return RandomFloat(0, 1);
 }
 
-Float32 DRand::FastRandom32::RandomFloat(Float32 min, Float32 max)
+Float32 FastRandom32::RandomFloat(Float32 min, Float32 max)
 {
     return min + ((max - min) * ((Float32) rand32() / (Float32)RAND_MAX));
 }
 
-void DRand::FastRandom32::DiscardRandom(UInt32 nTimes)
+void FastRandom32::DiscardRandom(UInt32 nTimes)
 {
     int q = 0;
     for (auto i = 0u; i < nTimes; i++)
         q = rand32();
 }
 
-DRand::MersenneRandom64::MersenneRandom64()
+MersenneRandom64::MersenneRandom64()
 {
     UInt64 seed = rand64.default_seed;
 
-    if (DRand::bAlwaysRandomizeSeed) {
+    if (bAlwaysRandomizeSeed) {
         srand(_time32(nullptr));
         seed = rand64() ^ 0xC;
     }
@@ -66,42 +66,42 @@ DRand::MersenneRandom64::MersenneRandom64()
     rand64.seed(seed);
 }
 
-DRand::MersenneRandom64::MersenneRandom64(UInt64 seed)
+MersenneRandom64::MersenneRandom64(UInt64 seed)
 {
     rand64.seed(seed);
 }
 
-UInt64 DRand::MersenneRandom64::RandomUInt()
+UInt64 MersenneRandom64::RandomUInt()
 {
     return (UInt64) rand64();
 }
 
-UInt64 DRand::MersenneRandom64::RandomUInt(UInt64 min, UInt64 max)
+UInt64 MersenneRandom64::RandomUInt(UInt64 min, UInt64 max)
 {
     return (UInt64) rand64() % (max - min) + min;
 }
 
-Int64 DRand::MersenneRandom64::RandomInt()
+Int64 MersenneRandom64::RandomInt()
 {
     return (Int64) rand64();
 }
 
-Int64 DRand::MersenneRandom64::RandomInt(Int64 min, Int64 max)
+Int64 MersenneRandom64::RandomInt(Int64 min, Int64 max)
 {
     return (Int64) rand64() % (max - min) + min;
 }
 
-Float64 DRand::MersenneRandom64::RandomFloat()
+Float64 MersenneRandom64::RandomFloat()
 {
     return RandomFloat(0, 1);
 }
 
-Float64 DRand::MersenneRandom64::RandomFloat(Float64 min, Float64 max)
+Float64 MersenneRandom64::RandomFloat(Float64 min, Float64 max)
 {
     return min + ((max - min) * ((Float64)rand64() / (Float64)RAND_MAX));
 }
 
-void DRand::MersenneRandom64::DiscardRandom(UInt64 nTimes)
+void MersenneRandom64::DiscardRandom(UInt64 nTimes)
 {
     {
         int q = 0;
--- DiabolicalEngine/src/Utility/Random.h
+++ DiabolicalEngine/src/Utility/Random.h
@@ -2,42 +2,40 @@
 #include "..\DiabolicalTypes.h"
 #include <random>
 #include <time.h>
-namespace DRand
+static bool bAlwaysRandomizeSeed = true;
+class FastRandom32
 {
-	static bool bAlwaysRandomizeSeed = true;
-	class FastRandom32
-	{
-		FastRandom32();
-		FastRandom32(UInt32 seed);
+	FastRandom32();
+	FastRandom32(UInt32 seed);
 
-		UInt32 RandomUInt();
-		UInt32 RandomUInt(UInt32 min, UInt32 max);
+	UInt32 RandomUInt();
+	UInt32 RandomUInt(UInt32 min, UInt32 max);
 
-		Int32 RandomInt();
-		Int32 RandomInt(Int32 min, Int32 max);
+	Int32 RandomInt();
+	Int32 RandomInt(Int32 min, Int32 max);
 
-		Float32 RandomFloat();
-		Float32 RandomFloat(Float32 min, Float32 max);
+	Float32 RandomFloat();
+	Float32 RandomFloat(Float32 min, Float32 max);
 
-		void DiscardRandom(UInt32 nTimes);
-		std::minstd_rand rand32;
-	};
+	void DiscardRandom(UInt32 nTimes);
+	std::minstd_rand rand32;
+};
 
-	class MersenneRandom64
-	{
-		MersenneRandom64();
-		MersenneRandom64(UInt64 seed);
+class MersenneRandom64
+{
+	MersenneRandom64();
+	MersenneRandom64(UInt64 seed);
 
-		UInt64 RandomUInt();
-		UInt64 RandomUInt(UInt64 min, UInt64 max);
+	UInt64 RandomUInt();
+	UInt64 RandomUInt(UInt64 min, UInt64 max);
 
-		Int64 RandomInt();
-		Int64 RandomInt(Int64 min, Int64 max);
+	Int64 RandomInt();
+	Int64 RandomInt(Int64 min, Int64 max);
 
-		Float64 RandomFloat();
-		Float64 RandomFloat(Float64 min, Float64 max);
+	Float64 RandomFloat();
+	Float64 RandomFloat(Float64 min, Float64 max);
 
-		void DiscardRandom(UInt64 nTimes);
-		std::mt19937_64 rand64;
-	};
+	void DiscardRandom(UInt64 nTimes);
+	std::mt19937_64 rand64;
+};
 };
