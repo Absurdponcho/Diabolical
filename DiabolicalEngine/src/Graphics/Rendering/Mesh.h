5ea0d07f1094ce4a8696a168901cee227e44023f
Absurdponcho <60100776+Absurdponcho@users.noreply.github.com>
2022-02-24 21:10:08 +1100

    cube primitive

--- DiabolicalEngine/src/Graphics/Rendering/Mesh.h
+++ DiabolicalEngine/src/Graphics/Rendering/Mesh.h
@@ -1,11 +1,26 @@
 #pragma once
 #include "Types/DVector.h"
+#include "Maths/Maths.h"
 
 class DMesh
 {
 public:
+	DMesh() {}
+
+	DMesh(const DVector<Vector3>& NewVertices, const DVector<Vector3>& NewNormals, const DVector<int>& NewIndices, const DVector<Vector2>& NewUVs)
+		:	Vertices(NewVertices), Normals(NewNormals), Indices(NewIndices), UVs(NewUVs)
+	{}
+
+	inline const DVector<Vector3>& GetVertices() { return Vertices; }
+	inline const DVector<Vector3>& GetNormals() { return Normals; }
+	inline const DVector<int>& GetIndices() { return Indices; }
+	inline const DVector<Vector2>& GetUVs() { return UVs; }
+
 
 protected:
-
+	DVector<Vector3> Vertices;
+	DVector<Vector3> Normals;
+	DVector<int> Indices;
+	DVector<Vector2> UVs;
 };
 
--- DiabolicalEngine/src/Graphics/Rendering/MeshPrimitives.h
+++ DiabolicalEngine/src/Graphics/Rendering/MeshPrimitives.h
@@ -1,2 +1,7 @@
 #pragma once
 #include "Mesh.h"
+
+namespace MeshPrimitives
+{
+	const DMesh Cube;
+}
